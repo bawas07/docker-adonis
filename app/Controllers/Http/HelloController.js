693f249ee5b62255a28caf9883082f2497e59c30
bawas07 <bayu.waskita.07@gmail.com>
2020-11-14 09:09:10 +0700

    test console

--- app/Controllers/Http/HelloController.js
+++ app/Controllers/Http/HelloController.js
@@ -5,6 +5,7 @@ const Logger = use('Logger')
 class HelloController {
     ping ({request, response}) {
         Logger.info('You are pinging to this app')
+        console.log('aha')
         response.json({
             "you-said": "ping",
             "i-said": "pong"
